fd5dd5427b469a9b6c82c3956aa790e486c42003
deathcap <deathcap@gmx.co.uk>
2014-02-24 20:30:11 -0800

    Clear data when block is destroyed. Closes GH-2

--- index.js
+++ index.js
@@ -5,21 +5,38 @@ module.exports = function(game, opts) {
 };
 
 function BlockData(game, opts) {
-  /*jshint unused:false */
   this.game = game;
+
+  this.enable();
 }
+
+BlockData.prototype.enable = function() {
+  var self = this;
+
+  this.game.on('setBlock', this.onSetBlock = function(target) {
+    // clear blockdata when blocks change
+    self.clear.apply(self, target);
+  });
+};
+
+BlockData.prototype.disable = function() {
+  this.game.removeListener('setBlock', this.onSetBlock);
+};
+
+
+// API
 
 // Get the blockdataMap for the chunk at given world coordinates
 // Returns undefined if the chunk doesn't exist
-// Initializes blockdataMap to {} if it doesn't exist (but the chunk does)
-BlockData.prototype.getForChunk = function(x, y, z) {
+// Initializes blockdataMap to {} if it doesn't exist (but the chunk does), _unless_ noCreate true
+BlockData.prototype.getForChunk = function(x, y, z, noCreate) {
   var chunkIndex = this.game.voxels.chunkAtCoordinates(x, y, z).join('|');
   var chunk = this.game.voxels.chunks[chunkIndex];
 
   if (!chunk)
     return undefined;
   
-  if (chunk.blockdata === undefined)
+  if (chunk.blockdata === undefined && !noCreate)
     chunk.blockdata = {};
 
   return chunk.blockdata;
@@ -32,27 +49,21 @@ BlockData.prototype.coordsToKey = function(x, y, z) {
 
 BlockData.prototype.get = function(x, y, z) {
   var blockdataMap = this.getForChunk(x, y, z);
-
-  if (blockdataMap === undefined)
-    return undefined;
+  if (blockdataMap === undefined) return undefined;
 
   return blockdataMap[this.coordsToKey(x, y, z)];
 };
 
 BlockData.prototype.set = function(x, y, z, data) {
   var blockdataMap = this.getForChunk(x, y, z);
-
-  if (blockdataMap === undefined)
-    return undefined;
+  if (blockdataMap === undefined) return undefined;
 
   blockdataMap[this.coordsToKey(x, y, z)] = data;
 };
 
 BlockData.prototype.clear = function(x, y, z) {
-  var blockdataMap = this.getForChunk(x, y, z);
-
-  if (blockdataMap === undefined)
-    return undefined;
+  var blockdataMap = this.getForChunk(x, y, z, true); // don't create for chunk if doesn't exist (read-only)
+  if (blockdataMap === undefined) return undefined;
 
   delete blockdataMap[this.coordsToKey(x, y,z)];
 };
